01fe9ad60bedfe8867e5d9adfb60beee0cebef66
George Serdyukov <glossy.mortal@gmail.com>
2022-06-22 16:14:14 +0300

    Детализирован класс Лампа. Добавил новые состояния и поведения лампы.

--- src/main/java/lamp/Lamp.java
+++ src/main/java/lamp/Lamp.java
@@ -6,50 +6,118 @@ class Lamp {
 
     private boolean isBroken;
 
+    private int usageResource;
+
     private int turnOnCounter;
 
+    private final int voltageLamp;
+
+    private int voltagePower;
+
     private String color;
+
+
+    public Lamp(String color, int voltageLamp, int usageResource) {
+        isOn = false;
+        isBroken = false;
+        voltagePower = 220;
+        this.color = color;
+        this.voltageLamp = voltageLamp;
+        this.usageResource = usageResource;
+    }
+
+    public void turnOn() {
+        if (!isBroken || turnOnCounter != usageResource || voltagePower == voltageLamp) {
+            isOn = true;
+            turnOnCounter++;
+        } else {
+            brokeLamp();
+        }
+        if (voltagePower > voltageLamp) {
+            brokeLamp();
+        }
+        if (voltagePower < voltageLamp) {
+            turnOff();
+        }
+    }
+
+    public void turnOff() {
+        if (!isBroken) {
+            isOn = false;
+        }
+    }
+
+    private void brokeLamp() {
+        isBroken = true;
+        isOn = false;
+    }
+
+    public void lampInfo() {
+        System.out.println("Характеристика лампы:" +
+                "\n Ресурс использования: " + getUsageResource() +
+                "\n Напряжения питания : " + getVoltageLamp() +
+                "\n Цвет лампы : " + getColor());
+    }
+
+    public void lampState() {
+        if (isOn) {
+            System.out.println("Лампа включена.");
+        } else if (voltagePower < voltageLamp) {
+            System.out.println("Лампа выключена. Не хватает напряжения.");
+        } else {
+            System.out.println("Лампа выключена.");
+        }
+
+        if (isBroken && turnOnCounter == usageResource) {
+            System.out.println("Лампа сломана. Ресурс лампы выработан");
+        }
+
+        if (isBroken && voltagePower > voltageLamp) {
+            System.out.println("Лампа сломана из-за высокого напряжения");
+        }
+    }
+
+    public int getUsageResource() {
+        return usageResource;
+    }
+
+    public void setUsageResource(int usageResource) {
+        this.usageResource = usageResource;
+    }
+
+
+    public int getTurnOnCounter() {
+        return turnOnCounter;
+    }
+
+    public void setTurnOnCounter(int turnOnCounter) {
+        this.turnOnCounter = turnOnCounter;
+        if (usageResource == turnOnCounter) {
+            brokeLamp();
+        } else {
+            isOn = true;
+        }
+    }
+
+    public int getVoltageLamp() {
+        return voltageLamp;
+    }
+
+    public void setVoltagePower(int voltagePower) {
+        this.voltagePower = voltagePower;
+        if (voltageLamp != voltagePower) {
+            brokeLamp();
+        }
+    }
 
     public String getColor() {
         return color;
     }
 
     public void setColor(String color) {
-        this.color = color;
-    }
-
-    public Lamp(String color) {
-        isOn = false;
-        isBroken = false;
-        this.color = color;
-    }
-
-    public void turnOn() {
-    isOn = true;
-    turnOnCounter++;
-    if (turnOnCounter == 3){
-        brokeLamp();
-    }
-    }
-
-    public void brokeLamp(){
-        isBroken = true;
-        isOn = false;
-    }
-
-    public void turnOff() {
-    isOn = false;
-    }
-
-    public void lampInfo(){
-        if(isOn){
-            System.out.println("Лампа цвета - " + getColor() + " включена.");
-        }else {
-            System.out.println("Лампа цвета - " + getColor() + " выключена.");
+        if (!isBroken) {
+            this.color = color;
         }
-        if(isBroken){
-            System.out.println("Лампа цвета - " + getColor() + " сломана.");
-        }
-
     }
+
 }
--- src/main/java/lamp/Program.java
+++ src/main/java/lamp/Program.java
@@ -1,23 +1,48 @@
 package main.java.lamp;
 
 public class Program {
-    public static void main(String[] args){
-        Lamp simpleLamp = new Lamp("Blue");
+    public static void main(String[] args) {
+        Lamp simpleLamp = new Lamp("Blue", 220, 10);
 
         simpleLamp.lampInfo();
+        System.out.println("");
 
         simpleLamp.turnOn();
-        simpleLamp.lampInfo();
+        simpleLamp.lampState();
 
+        System.out.println("");
         simpleLamp.turnOff();
-        simpleLamp.lampInfo();
+        simpleLamp.lampState();
+
+        System.out.println("");
 
         simpleLamp.setColor("Yellow");
 
-        simpleLamp.turnOn();
         simpleLamp.lampInfo();
+        System.out.println("");
 
         simpleLamp.turnOn();
-        simpleLamp.lampInfo();
+        simpleLamp.lampState();
+        System.out.println("");
+
+        simpleLamp.turnOn();
+        simpleLamp.lampState();
+        System.out.println("");
+
+        simpleLamp.setTurnOnCounter(10);
+        simpleLamp.lampState();
+        System.out.println("");
+
+        simpleLamp.setTurnOnCounter(1);
+        simpleLamp.lampState();
+        System.out.println("");
+
+        simpleLamp.setVoltagePower(230);
+        simpleLamp.lampState();
+
+        System.out.println("");
+
+        simpleLamp.setVoltagePower(210);
+        simpleLamp.lampState();
     }
 }
